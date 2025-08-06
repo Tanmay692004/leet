005154606ad374275dadccbe3a698865e1955151
Tanmay692004 <tanmaybinjola0@gmail.com>
2025-08-06 15:26:06 +0530

    easy shih

---
+++ 1672.richest-customer-wealth.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1672 lang=python3
+#
+# [1672] Richest Customer Wealth
+#
+
+# @lc code=start
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxacc = []
+        for  i in range(len(accounts)):
+            maxacc.append(sum ( accounts[i]))
+        return max(maxacc)
+# @lc code=end
+
