31b46bc68881a03b666d59ed42b4dd4c4bafb291
Tanmay692004 <tanmaybinjola0@gmail.com>
2025-08-11 23:52:30 +0530

    ok somehow easier than the easy one

---
+++ 12.integer-to-roman.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=12 lang=python3
+#
+# [12] Integer to Roman
+#
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], ["CD", 400],["D", 500],["CM", 900],["M", 1000]]
+        res= ""
+        for sym, val in symList[::-1]:
+            if num// val:
+                count = num//val
+                res+= count*sym
+                num%=val
+        return res
+# @lc code=end
+
